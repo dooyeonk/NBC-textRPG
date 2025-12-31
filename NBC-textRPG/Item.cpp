827f765c94a334eb484e18d779a7ecbc07bf1306
ssh1202 <tmdghks9812@gmail.com>
2025-12-31 15:12:29 +0900

    add : 아이템 클래스 추가

--- NBC-textRPG/Item.cpp
+++ NBC-textRPG/Item.cpp
@@ -1,1 +1,35 @@
 #include "Item.h"
+#include <string>
+using namespace std;
+
+Item::Item() : use(false)  // 아이템 클래스의 생성자. use의 bool을 false로
+{
+}
+
+Item::Item(const string& itemName, bool itemUse) : name(itemName), use(itemUse)
+{
+}
+
+Item::~Item()
+{
+}
+
+string Item::getItemName() // 타입을 제일 앞으로
+{
+    return name;
+}
+
+bool Item::getUse()
+{
+    return use;
+}
+
+void Item::setItemName(const string& itemName)
+{
+    name = itemName;
+}
+
+void Item::setUse(bool itemUse)
+{
+    use = itemUse;
+}
--- NBC-textRPG/Item.h
+++ NBC-textRPG/Item.h
@@ -1,4 +1,24 @@
 #pragma once
-class Item
+#include <string>
+
+class Item // 기본 클래스
 {
+private: // 멤버 변수
+    std::string name;
+    bool use;
+
+public: // 멤버 함수
+    Item(); // 생성자
+    Item(const std::string& itemName, bool itemUse);
+    ~Item(); // 소멸자
+
+
+    std::string getItemName(); // getter 
+    bool getUse();
+
+    void setItemName(const std::string& itemName); // setter
+    void setUse(bool itemUse);
+
+};
+
 };
