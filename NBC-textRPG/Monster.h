862076626a78485c7b4b1e0db08809a98a6dfa99
Y2Y <mio970407@gmail.com>
2025-12-31 19:14:25 +0900

    add:몬스터클래스

--- NBC-textRPG/Monster.h
+++ NBC-textRPG/Monster.h
@@ -1,4 +1,49 @@
 #pragma once
+#include <string>
+#include <iostream>
+
 class Monster
 {
+private:
+    std::string monName;
+    int healthPoint;
+    int attack;
+    
+public:
+    Monster()
+    {
+
+        std::cout << monName << "가 나타났다!" << std::endl;
+    }
+    void hpDamaged(int damage)
+    {
+        if (damage > healthPoint)
+        {
+            healthPoint = 0;
+        }
+        else
+        {
+            healthPoint -= damage;
+        }
+    }
+    int getMonHealthPoint()
+    {
+        return healthPoint;
+    }
+    bool isDead()
+    {
+        if (healthPoint == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    ~Monster()
+    {
+        std::cout << monName << "가 쓰러졌다." << std::endl;
+    }
 };
