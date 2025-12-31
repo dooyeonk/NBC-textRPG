3227490d747509159b3108fcea493a7144debf26
강두연 <dyhouse0517@gmail.com>
2025-12-31 18:09:18 +0900

    add: 캐릭터 클래스 헤더 및 구현부 추가

--- NBC-textRPG/Character.cpp
+++ NBC-textRPG/Character.cpp
@@ -1,1 +1,155 @@
 #include "Character.h"
+#include <algorithm>
+
+Character* Character::instance = nullptr;
+
+Character::Character(std::string name, int healthPoint, int attackPower)
+    : name(name), hp(healthPoint), maxHp(healthPoint), attackPower(attackPower), level(1), experience(0),
+      maxExperience(100), gold(0)
+{
+}
+
+Character::~Character()
+{
+}
+
+Character* Character::getInstance(std::string name, int healthPoint, int attackPower)
+{
+    if (instance == nullptr)
+    {
+        instance = new Character(name, healthPoint, attackPower);
+    }
+    return instance;
+}
+
+void Character::destroy()
+{
+    if (instance != nullptr)
+    {
+        delete instance;
+        instance = nullptr;
+    }
+}
+
+bool Character::isDead() const
+{
+    return hp <= 0;
+}
+
+void Character::gainExperience(int amount)
+{
+    experience += amount;
+    if (experience >= maxExperience)
+    {
+        levelUp();
+    }
+}
+
+void Character::levelUp()
+{
+    if (level >= 10)
+    {
+        std::cout << "이미 최대 레벨입니다." << std::endl;
+        return;
+    }
+    experience = 0;
+    level++;
+    maxHp += 20;
+    hp = maxHp; // 레벨업 시 체력 풀회복
+    attackPower += 5;
+
+    std::cout << "=== LEVEL UP! 현재 레벨: " << level << " ===" << std::endl;
+}
+
+void Character::addGold(int amount)
+{
+    gold += amount;
+}
+
+void Character::addItem(Item* newItem)
+{
+    if (!newItem)
+    {
+        return;
+    }
+
+    for (auto& slot : inventory)
+    {
+        if (slot.item->getName() == newItem->getName())
+        {
+            slot.quantity++;
+            std::cout << newItem->getName() << "의 수량이 증가했습니다." << std::endl;
+            return;
+        }
+    }
+
+    inventory.push_back(InventoryItem(newItem));
+}
+
+void Character::setHp(int amount)
+{
+    // 0과 maxHealthPoint 사이로 값 제한
+    hp = std::max(0, std::min(amount, maxHp));
+}
+
+void Character::displayStatus() const
+{
+    std::cout << "\n----------------------------" << std::endl;
+    std::cout << "이름: " << name << " (Lv." << level << ")" << std::endl;
+    std::cout << "HP: " << hp << " / " << maxHp << std::endl;
+    std::cout << "공격력: " << attackPower << " | 골드: " << gold << std::endl;
+    std::cout << "----------------------------\n" << std::endl;
+}
+
+void Character::reduceItem(const std::string& itemName)
+{
+    for (auto iterator = inventory.begin(); iterator != inventory.end(); ++iterator)
+    {
+        if (iterator->item->getName() == itemName)
+        {
+
+            iterator->quantity--;
+            if (iterator->quantity <= 0)
+            {
+                delete iterator->item;
+                inventory.erase(iterator);
+            }
+            return;
+        }
+    }
+}
+
+std::string Character::getName() const
+{
+    return name;
+}
+
+int Character::getHp() const
+{
+    return hp;
+}
+
+int Character::getMaxHp() const
+{
+    return maxHp;
+}
+
+int Character::getMaxExperience() const
+{
+    return maxExperience;
+}
+
+int Character::getAttackPower() const
+{
+    return attackPower;
+}
+
+int Character::getExperience() const
+{
+    return experience;
+}
+
+const std::vector<InventoryItem>& Character::getInventory() const
+{
+    return inventory;
+}
--- NBC-textRPG/Character.h
+++ NBC-textRPG/Character.h
@@ -1,4 +1,73 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Item.h"
+
+struct InventoryItem
+{
+    Item* item;
+    int quantity;
+
+    InventoryItem(Item* item) : item(item), quantity(1)
+    {
+    }
+};
+
+struct Stats
+{
+    int hp = 0;
+    int attack = 0;
+};
+
 class Character
 {
+public:
+    static Character* instance;
+
+    static Character* getInstance(std::string name = "Student", int hp = 200, int atk = 30);
+    static void destroy();
+
+    bool isDead() const;
+
+    void gainExperience(int amount);
+    void levelUp();
+    void addGold(int amount);
+    void addItem(Item* newItem);
+    void reduceItem(const std::string& itemName);
+
+    void setHp(int amount);
+
+    void displayStatus() const;
+
+    std::string getName() const;
+
+    int getHp() const;
+    int getMaxHp() const;
+    int getAttackPower() const;
+    int getExperience() const;
+    int getMaxExperience() const;
+
+    const std::vector<InventoryItem>& getInventory() const;
+
+private:
+    Character(std::string name, int healthPoint, int attackPower);
+    ~Character();
+
+    Character(const Character&) = delete;
+    Character& operator=(const Character&) = delete;
+
+    const std::string name;
+
+    int hp;
+    int maxHp;
+    int attackPower;
+    int level;
+    int experience;
+    int maxExperience;
+    int gold;
+
+    std::vector<InventoryItem> inventory;
 };
