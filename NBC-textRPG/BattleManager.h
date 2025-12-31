4397d865578d6917494bab658e89755bde72fd06
강두연 <dyhouse0517@gmail.com>
2025-12-31 18:09:55 +0900

    add: 배틀매니저 클래스 헤더  구현부 추가

--- NBC-textRPG/BattleManager.h
+++ NBC-textRPG/BattleManager.h
@@ -1,4 +1,33 @@
 #pragma once
+
+#include <string>
+
+#include "Character.h"
+#include "Monster.h"
+
+enum class BattleResult
+{
+    VICTORY,
+    DEFEAT,
+};
+
+struct BattleReport
+{
+    BattleResult result;
+    int experience;
+    int gold;
+    std::string monsterName;
+    std::string droppedItem;
+};
+
 class BattleManager
 {
+public:
+    BattleReport battle(Character& character);
+
+private:
+    void executePlayerTurn(Character& character, Monster& monster, int& attackBonus);
+    void executeMonsterTurn(Character& character, Monster& monster);
+    void processVictory(Character& character, BattleReport& report);
+    bool itemUsed(Character& character, int& attackBonus);
 };
