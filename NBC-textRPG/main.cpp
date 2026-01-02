5f1785f5f5640232ca9d4c29b3640930bafa3165
Sangmin Kim <sm524233@naver.com>
2026-01-02 18:26:20 +0900

    add : Logger 클래스 추가
    1. Logger클래스 틀만 추가했습니다
    2. 와 샌즈! 출력 함수 만들었습니다
    3. 한글 출력 관련해서 main에 2줄 추가했습니다

--- NBC-textRPG/main.cpp
+++ NBC-textRPG/main.cpp
@@ -2,9 +2,14 @@
 #include "GameManager.h"
 #include <cstdlib>
 #include <ctime>
+#include <windows.h>
 
 int main()
 {
+    // 한글 출력을 위한 설정
+    SetConsoleOutputCP(CP_UTF8);
+    setvbuf(stdout, nullptr, _IOFBF, 1000);
+
     srand(time(NULL));
     GameManager gm;
     gm.run();
