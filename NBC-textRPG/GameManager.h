66fdeba2ba5ed6ded79a50da3965ecdc45c335c9
Sangmin Kim <sm524233@naver.com>
2026-01-02 12:18:23 +0900

    update : run() 쪽 간소화
    1. run() 쪽 BattleReport 받아와서 하도록 간소화했습니다
    2. 함수/변수이름 camelCase로 수정했습니다
    3. Character에서 getLevel() 하나 필요해서 만들었습니다

--- NBC-textRPG/GameManager.h
+++ NBC-textRPG/GameManager.h
@@ -1,4 +1,30 @@
 #pragma once
+
+class Character;
+class Monster;
+class BattleManager;
+
 class GameManager
 {
+public:
+    GameManager();
+    ~GameManager();
+
+    void run();
+
+private:
+
+    // 상점 ㄱㄱ
+    void visitShop(Character* player);
+
+    // 엔딩 처리 (보스 클리어 시)
+    void ending();
+
+    // 전투 승리 후 보여줄 선택지
+    void showPostBattleMenu();
+
+private:
+    Character* player;
+    BattleManager* battleMg;
+    bool bIsGameOver;
 };
