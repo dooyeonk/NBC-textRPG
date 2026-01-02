383b39607e2d1f80bdea815513e7d61c82accc1c
mio970407-dot <mio970407@gmail.com>
2026-01-02 10:19:09 +0900

    add: 몬스터클래스 (#12)

--- NBC-textRPG/Monster.cpp
+++ NBC-textRPG/Monster.cpp
@@ -1,1 +1,29 @@
 #include "Monster.h"
+void Monster::hpDamaged(int damage)
+{
+    if (damage > healthPoint)
+    {
+        healthPoint = 0;
+        return;
+    }
+    healthPoint -= damage;
+}
+int Monster::getHealthPoint()
+{
+    return healthPoint;
+}
+int Monster::getAttack()
+{
+    return attack;
+}
+bool Monster::isDead()
+{
+    if (healthPoint == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
--- NBC-textRPG/Monster.h
+++ NBC-textRPG/Monster.h
@@ -1,4 +1,23 @@
 #pragma once
+#include <string>
+#include <iostream>
+
 class Monster
 {
+private:
+    std::string name;
+    int healthPoint;
+    int attack;
+
+public:
+    Monster()
+    {
+    }
+    void hpDamaged(int damage);
+    int getHealthPoint();
+    int getAttack();
+    bool isDead();
+    virtual ~Monster()
+    {
+    }
 };
