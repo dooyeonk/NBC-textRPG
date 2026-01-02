16ceb690d789be086df3fb95ecbc77ac7d775486
Y2Y <mio970407@gmail.com>
2026-01-02 10:13:34 +0900

    add 260102

--- NBC-textRPG/Monster.h
+++ NBC-textRPG/Monster.h
@@ -13,83 +13,10 @@ public:
     Monster()
     {
     }
-    void hpDamaged(int damage)
-    {
-        if (damage > healthPoint)
-        {
-            healthPoint = 0;
-            return;
-        }
-        healthPoint -= damage;
-    }
-    int getHealthPoint()
-    {
-        return healthPoint;
-    }
-    int getAttack()
-    {
-        return attack;
-    }
-    bool isDead()
-    {
-        if (healthPoint == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    virtual ~Monster()
-    {
-    }
-};
-#pragma once
-#include <string>
-#include <iostream>
-
-class Monster
-{
-private:
-    std::string name;
-    int healthPoint;
-    int attack;
-
-public:
-    Monster()
-    {
-    }
-    void hpDamaged(int damage)
-    {
-        if (damage > healthPoint)
-        {
-            healthPoint = 0;
-            return;
-        }
-        healthPoint -= damage;
-    }
-    int getHealthPoint()
-    {
-        return healthPoint;
-    }
-    int getAttack()
-    {
-        return attack;
-    }
-    bool isDead()
-    {
-        if (healthPoint == 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
+    void hpDamaged(int damage);
+    int getHealthPoint();
+    int getAttack();
+    bool isDead();
     virtual ~Monster()
     {
     }
