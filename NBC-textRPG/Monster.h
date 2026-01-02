49beb61e9d995c9487618ed6b708700397882441
Y2Y <mio970407@gmail.com>
2026-01-02 09:57:24 +0900

    update 260102

--- NBC-textRPG/Monster.h
+++ NBC-textRPG/Monster.h
@@ -5,30 +5,30 @@
 class Monster
 {
 private:
-    std::string monName;
+    std::string name;
     int healthPoint;
     int attack;
-    
+
 public:
     Monster()
     {
-
-        std::cout << monName << "가 나타났다!" << std::endl;
     }
     void hpDamaged(int damage)
     {
         if (damage > healthPoint)
         {
             healthPoint = 0;
+            return;
         }
-        else
-        {
-            healthPoint -= damage;
-        }
+        healthPoint -= damage;
     }
-    int getMonHealthPoint()
+    int getHealthPoint()
     {
         return healthPoint;
+    }
+    int getAttack()
+    {
+        return attack;
     }
     bool isDead()
     {
@@ -42,8 +42,55 @@ public:
         }
     }
 
-    ~Monster()
+    virtual ~Monster()
     {
-        std::cout << monName << "가 쓰러졌다." << std::endl;
+    }
+};
+#pragma once
+#include <string>
+#include <iostream>
+
+class Monster
+{
+private:
+    std::string name;
+    int healthPoint;
+    int attack;
+
+public:
+    Monster()
+    {
+    }
+    void hpDamaged(int damage)
+    {
+        if (damage > healthPoint)
+        {
+            healthPoint = 0;
+            return;
+        }
+        healthPoint -= damage;
+    }
+    int getHealthPoint()
+    {
+        return healthPoint;
+    }
+    int getAttack()
+    {
+        return attack;
+    }
+    bool isDead()
+    {
+        if (healthPoint == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    virtual ~Monster()
+    {
     }
 };
