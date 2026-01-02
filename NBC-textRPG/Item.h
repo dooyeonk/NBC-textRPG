22fbe7cb38efb04233251ac8b70b0b92e274ad8d
ssh1202 <tmdghks9812@gmail.com>
2026-01-02 10:29:11 +0900

    add : 아이템 클래스 추가 (#10)

--- NBC-textRPG/Item.h
+++ NBC-textRPG/Item.h
@@ -1,4 +1,54 @@
 #pragma once
+#include <string>
+
+struct ItemEffect
+{
+    int hp = 0;
+    int attackPower = 0;
+};
+
 class Item
 {
+private:
+    std::string name;
+    int price;
+
+public:
+    Item();
+    Item(const std::string& itemName, int itemPrice);
+    virtual ~Item() = default;
+
+    virtual ItemEffect getEffect() = 0;
+
+    std::string getName();
+    int getPrice();
+
+    void setName(const std::string& itemName);
+    void setPrice(int itmePrice);
+};
+
+class HealthPotion : public Item
+{
+private:
+    int heal;
+
+public:
+    HealthPotion();
+    HealthPotion(const std::string& itemName, int itemPrice, int healing);
+    ~HealthPotion();
+
+    ItemEffect getEffect();
+};
+
+class AttackBoost : public Item
+{
+private:
+    int attackUp;
+
+public:
+    AttackBoost();
+    AttackBoost(const std::string& itemName, int itemPrice, int atkUp);
+    ~AttackBoost();
+
+    ItemEffect getEffect();
 };
