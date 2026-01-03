05b1c36131885d34410d89636e32d52574fe4f68
0Kaede0 <hun151800@gmail.com>
2026-01-04 07:05:07 +0900

    add: 보스몬스터와 나머지 몬스터들 해봤습니다

--- NBC-textRPG/MonsterBoss.h
+++ NBC-textRPG/MonsterBoss.h
@@ -9,14 +9,24 @@
 class MonsterBoss : public Monster
 {
 private:
-    int specialHp;
+    int maxTotalHp;// 최대체력에서 10%계산용
 
 public:
     
-    MonsterBoss(int characterLevel);
+    MonsterBoss(int characterLevel) : Monster(characterLevel)
+    {
+        name = "언리얼 C++";
+
+        maxHp = static_cast<int>(maxHp * 1.5);
+        hp = maxHp;//부모 클래스에서 만든 체력에 1.5배(보스 전용체력)
+
+        attackPower = static_cast<int>(attackPower * 1.5); // 1.5배 공격력
+
+        maxTotalHp = maxHp;
+    }
 
     void hpDamaged(int damage);
-    int getHealthPonint();
+    int getHealthPoint();
     int getAttack();
     bool isDead();
     std::string getName();
