a2f685f441353357c54231471d50a90dabf0085e
0Kaede0 <hun151800@gmail.com>
2026-01-04 07:05:07 +0900

    add: 보스몬스터와 나머지 몬스터들 해봤습니다

--- NBC-textRPG/MonsterBoss.cpp
+++ NBC-textRPG/MonsterBoss.cpp
@@ -2,7 +2,43 @@
 #include <cstdlib>
 #include <string>
 
-MonsterBoss::MonsterBoss(int caharcterLevel) : Monster(caharcterLevel)
+void MonsterBoss::hpDamaged(int damage)
 {
-    
+    hp -= damage;
+    if (hp < 0)
+        hp = 0;
+}
+
+int MonsterBoss::getHealthPoint()
+{
+
+    if (hp > maxTotalHp * 0.1)
+    {
+        std::cout << name << "체력 : ???" << std::endl;
+    }
+    else
+    {
+        std::cout << name << "체력 : " << hp << std::endl;
+    }
+    return hp;
+}
+
+
+
+
+int MonsterBoss::getAttack()
+{
+    return attackPower;
+}
+
+
+bool MonsterBoss::isDead()
+{
+    return hp <= 0;
+}
+
+
+std::string MonsterBoss::getName()
+{
+    return name;
 }
