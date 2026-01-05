4b3f949d5534d718f1cda7ab39392a5d320aa0a6
강두연 <dyhouse0517@gmail.com>
2026-01-06 03:38:52 +0900

    update: 게임매니저 입력키 선택지 추가

--- NBC-textRPG/Core/GameManager/GameManager.cpp
+++ NBC-textRPG/Core/GameManager/GameManager.cpp
@@ -35,10 +35,13 @@ void GameManager::run()
     cin >> name;
     player = Character::getInstance(name); // 싱글톤
 
+    showPostBattleMenu();
+
     // 루프 시작
     while (!bIsGameOver)
     {
         system("cls");
+
         int currentLevel = player->getLevel();
 
         BattleReport report = battleMg->battle(*player);
@@ -52,6 +55,7 @@ void GameManager::run()
                 ending(report.monsterName);
                 break;
             }
+
             showPostBattleMenu();
         }
 
@@ -86,7 +90,8 @@ void GameManager::showPostBattleMenu()
 
     while (!bReadyForNextBattle)
     {
-        cout << "\n[정비 메뉴] 1.상점 방문  2.인벤토리 확인  3.스탯 확인  4.다음 전투(나가기): ";
+        cout << "\n[정비 메뉴] 1.상점 방문  2.인벤토리 확인  3.스탯 확인  4.다음 전투 시작  9. 난이도 변경  0. 게임 "
+                "종료: ";
         int choice;
         cin >> choice;
 
@@ -106,6 +111,15 @@ void GameManager::showPostBattleMenu()
         {
             cout << "전투 지역으로 이동합니다..." << endl;
             bReadyForNextBattle = true; // 메인 루프로!
+        }
+        else if (choice == 9)
+        {
+            battleMg->changeMode();
+        }
+        else if (choice == 0)
+        {
+            bIsGameOver = true;
+            break;
         }
         else
         {
@@ -186,9 +200,11 @@ void GameManager::ending(const string& bossName)
     cout << "\n[ Made by Team 3 ]" << endl;
     this_thread::sleep_for(chrono::seconds(1));
     cout << " - GameManager   : [김상민]" << endl;
+    cout << " - Logger        : [김상민 & 강두연]" << endl;
     cout << " - Battle System : [강두연]" << endl;
     cout << " - Character     : [성헌호 & 강두연]" << endl;
     cout << " - Monster       : [유주연 & 성헌호]" << endl;
+    cout << " - BossMonster   : [유주연 & 성헌호]" << endl;
     cout << " - Item          : [송승환]" << endl;
     cout << " - Shop          : [송승환]" << endl;
 
